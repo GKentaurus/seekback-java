42e25d0ce1951a4fd270ee43690fb044c81dd1c5
GKentaurus <gkent@outlook.com>
2020-11-17 11:57:46 -0500

    Interfaces DAO actualizadas para implementar JPA

---
+++ src/main/java/tech/seekback/dao/interfaces/BodegaProductosDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tech.seekback.dao.interfaces;
+
+import tech.seekback.dao.DAO;
+import tech.seekback.models.BodegaProductos;
+
+/**
+ *
+ * @author camorenoc
+ */
+public interface BodegaProductosDAO extends DAO<BodegaProductos, Integer> {
+
+}
---
+++ src/main/java/tech/seekback/dao/interfaces/RolesDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tech.seekback.dao.interfaces;
+
+import tech.seekback.dao.DAO;
+import tech.seekback.models.Roles;
+
+/**
+ *
+ * @author camorenoc
+ */
+public interface RolesDAO extends DAO<Roles, Integer> {
+}
