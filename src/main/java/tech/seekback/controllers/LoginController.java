21735e04ed1057c942532e3e1eb3878084a14393
danniela <danny94rh@gmail.com>
2021-04-27 22:49:02 -0500

    normalización de controller

--- src/main/java/tech/seekback/controllers/LoginController.java
+++ src/main/java/tech/seekback/controllers/LoginController.java
@@ -16,8 +16,29 @@ import javax.inject.Named;
 import javax.mail.MessagingException;
 import java.io.IOException;
 import java.io.Serializable;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 import java.util.Random;
+import javax.annotation.PostConstruct;
+import tech.seekback.dao.interfaces.CiudadDAO;
+import tech.seekback.dao.interfaces.DepartamentoDAO;
+import tech.seekback.models.Ciudad;
+import tech.seekback.models.Departamento;
+import tech.seekback.models.Direccion;
+import tech.seekback.models.Pais;
+import tech.seekback.models.Telefono;
+import tech.seekback.models.TipoDoc;
+import tech.seekback.models.templates.Timestamps;
+import tech.seekback.services.CiudadService;
+import tech.seekback.services.ClienteService;
+import tech.seekback.services.DireccionService;
+import tech.seekback.services.PaisService;
+import tech.seekback.services.RolService;
+import tech.seekback.services.TelefonoService;
+import tech.seekback.services.TipoDocService;
 
 /**
  * @author danny
@@ -27,13 +48,62 @@ import java.util.Random;
 public class LoginController extends CustomController implements Serializable {
 
   @EJB
+  private DepartamentoDAO departamentoDAO;
+
+  @EJB
+  private PaisService paisService;
+
+  @EJB
+  private CiudadDAO ciudadDAO;
+
+  @EJB
   private UsuarioService usuarioService;
+
+  @EJB
+  private TipoDocService tipoDocService;
+
+  @EJB
+  private RolService rolService;
+
+  @EJB
+  private CiudadService ciudadService;
+
+  @EJB
+  private DireccionService direccionService;
+
+  @EJB
+  private TelefonoService telefonoService;
+
+  @EJB
+  private CorreoService correoService;
+
+  @EJB
+  private ClienteService clienteService;
+
+  @EJB
+  private MailService mailService;
 
   @EJB
   private CorreoService correosService;
 
-  @EJB
-  private MailService mailService;
+  private Direccion direccion;
+  private Telefono telefono;
+  private Usuario cliente;
+
+  private Integer idPaisSeleccionado;
+  private List<Departamento> departamentoSeleccionado;
+
+  private List<Pais> paises;
+  private Integer idDepartamentoSeleccionado;
+  private List<Ciudad> ciudadSeleccionada;
+
+  private List<TipoDoc> tipoDocs;
+
+  private Integer idTipoDoc;
+  private Integer idCiudad;
+  private String phoneNumber;
+  private String address;
+  private String passwordConfirm;
 
   // TODO: Usar este boolean en la vista de perfil de usuario, para mostrar un mensaje que diga "Bienvenido de vuelta"
   private boolean cuentaReactivada = false;
@@ -43,13 +113,37 @@ public class LoginController extends CustomController implements Serializable {
   private String password;
   private String email;
 
-  //<editor-fold defaultstate="collapsed" desc="Getters && Setters">
-  public String getPassword() {
-    return password;
+  public LoginController() {
+    usuario = new Usuario();
+    direccion = new Direccion();
+    correo = new Correo();
+    telefono = new Telefono();
+    cliente = new Usuario();
   }
 
-  public void setPassword(String password) {
-    this.password = password;
+  //<editor-fold defaultstate="collapsed" desc="Getters && Setters">
+  public Usuario getUsuario() {
+    return usuario;
+  }
+
+  public void setUsuario(Usuario usuario) {
+    this.usuario = usuario;
+  }
+
+  public Integer getIdTipoDoc() {
+    return idTipoDoc;
+  }
+
+  public void setIdTipoDoc(Integer id) {
+    this.idTipoDoc = id;
+  }
+
+  public Integer getIdCiudad() {
+    return idCiudad;
+  }
+
+  public void setIdCiudad(Integer id) {
+    this.idCiudad = id;
   }
 
   public String getEmail() {
@@ -60,12 +154,36 @@ public class LoginController extends CustomController implements Serializable {
     this.email = email;
   }
 
-  public Usuario getUsuario() {
-    return usuario;
+  public String getPhoneNumber() {
+    return phoneNumber;
   }
 
-  public void setUsuario(Usuario usuario) {
-    this.usuario = usuario;
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getPasswordConfirm() {
+    return passwordConfirm;
+  }
+
+  public void setPasswordConfirm(String passwordConfirm) {
+    this.passwordConfirm = passwordConfirm;
   }
 
   public Correo getCorreo() {
@@ -105,6 +223,56 @@ public class LoginController extends CustomController implements Serializable {
       this.usuario.getTimestamps().resetDeleted_at();
       this.usuarioService.update(this.usuario);
       this.cuentaReactivada = true;
+    }
+  }
+
+  public void register() throws ConnectionExcep, NoSuchAlgorithmException, InvalidKeySpecException, IOException, MessagingException {
+    if (this.password.equals(this.passwordConfirm)) {
+      // Creación de Timestamp para todos los procesos
+      Timestamps timestamps = new Timestamps();
+      Date momentum = new Date();
+      timestamps.setCreated_at(momentum);
+      timestamps.setUpdated_at(momentum);
+
+      // Creación del objeto Usuario
+      this.usuario.setTipoDoc(tipoDocService.getOne(this.idTipoDoc));
+      this.usuario.setContrasena(password);
+      this.usuario.setRol(rolService.getOne(3));
+      this.usuario.setTimestamps(timestamps);
+      this.usuario = usuarioService.create(usuario);
+
+      // Creación del objeto Direccion
+      this.direccion.setUsuario(usuario);
+      this.direccion.setPseudonimo("Principal");
+      this.direccion.setDireccion(address);
+      this.direccion.setTelefono(phoneNumber);
+      this.direccion.setCiudad(ciudadService.getOne(idCiudad));
+      this.direccion.setEsPrincipal(true);
+      this.direccion.setTimestamps(timestamps);
+      this.direccion = direccionService.create(direccion);
+
+      // Creación del objeto Telefono
+      this.telefono.setUsuario(usuario);
+      this.telefono.setNumeroTelefono(phoneNumber);
+      this.telefono.setEsPrincipal(true);
+      this.telefono.setTimestamps(timestamps);
+      this.telefono = telefonoService.create(telefono);
+
+      // Creación del objeto Correo
+      this.correo.setUsuario(usuario);
+      this.correo.setCorreoElectronico(email);
+      this.correo.setEsPrincipal(true);
+      this.correo.setTimestamps(timestamps);
+      this.correo = correoService.create(correo);
+
+      this.mailService.sendEmail(
+              correo.getCorreoElectronico(),
+              "Bienvenido a Seekback",
+              "Su registro ha sido completado satisfactoriamente."
+      );
+
+      ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+      ec.redirect(ec.getRequestContextPath() + "/login.xhtml");
     }
   }
 
@@ -212,5 +380,76 @@ public class LoginController extends CustomController implements Serializable {
       ruta = "/frames/cuenta.xhtml";
     }
     ec.redirect(ec.getRequestContextPath() + ruta);
+  }
+
+  public Integer getIdPaisSeleccionado() {
+    return idPaisSeleccionado;
+  }
+
+  public void setIdPaisSeleccionado(Integer idPaisSeleccionado) {
+    try {
+      if (Objects.nonNull(idPaisSeleccionado)) {
+        departamentoSeleccionado = departamentoDAO
+                .getByIdPais(idPaisSeleccionado);
+      } else {
+        departamentoSeleccionado = null;
+      }
+    } catch (ConnectionExcep ex) {
+      ex.printStackTrace();
+    }
+    this.idPaisSeleccionado = idPaisSeleccionado;
+  }
+
+  public List<Departamento> getDepartamentoSeleccionado() {
+    return departamentoSeleccionado;
+  }
+
+  public List<Pais> getPaises() {
+    try {
+      if (Objects.isNull(paises)) {
+        paises = paisService.getAll();
+      }
+    } catch (ConnectionExcep ex) {
+      System.out.println("Error de la consulta 'paises'.");
+      ex.printStackTrace();
+    }
+    return paises;
+
+  }
+
+  public Integer getIdDepartamentoSeleccionado() {
+    return idDepartamentoSeleccionado;
+  }
+
+  public void setIdDepartamentoSeleccionado(Integer idDepartamentoSeleccionado) {
+    try {
+      if (Objects.nonNull(idDepartamentoSeleccionado)) {
+        ciudadSeleccionada = ciudadDAO
+                .getByIdDepartamento(idDepartamentoSeleccionado);
+      } else {
+        ciudadSeleccionada = null;
+      }
+    } catch (ConnectionExcep ex) {
+      ex.printStackTrace();
+    }
+    this.idDepartamentoSeleccionado = idDepartamentoSeleccionado;
+  }
+
+  public List<Ciudad> getCiudadSeleccionada() {
+    return ciudadSeleccionada;
+  }
+
+  @PostConstruct
+  public void init() {
+    try {
+      tipoDocs = tipoDocService.getAll();
+    } catch (ConnectionExcep ex) {
+      System.out.println("Error de la consulta 'TIPO DOC'.");
+      ex.printStackTrace();
+    }
+  }
+
+  public List<TipoDoc> getTipoDocs() {
+    return tipoDocs;
   }
 }
--- src/main/java/tech/seekback/controllers/UsuarioController.java
+++ src/main/java/tech/seekback/controllers/UsuarioController.java
@@ -29,37 +29,8 @@ public class UsuarioController extends CustomController implements Serializable 
   @EJB
   private UsuarioService usuarioService;
 
-  @EJB
-  private AdministradorService administradorService;
-
-  @EJB
-  private EmpleadoService empleadoService;
-
-  @EJB
-  private ClienteService clienteService;
-
-  @EJB
-  private DireccionService direccionService;
-
-  @EJB
-  private TelefonoService telefonoService;
-
-  @EJB
-  private CorreoService correoService;
-
-  @EJB
-  private TipoDocService tipoDocService;
-
   private Usuario usuario;
-  private Correo correo;
-  private Direccion direccion;
-  private Telefono telefono;
   private List<Usuario> usuarios;
-  private List<TipoDoc> tipoDocs;
-  private Integer idTipoDoc;
-  private String oldPassword;
-  private String newPassword;
-  private String confirmPassword;
 
   @PostConstruct
   public void init() {
@@ -75,39 +46,6 @@ public class UsuarioController extends CustomController implements Serializable 
     this.usuario = usuario;
   }
 
-  public Correo getCorreo() throws ConnectionExcep {
-    return correoService.getByIdPrincipal(usuario.getId());
-  }
-
-  public void setCorreo(Correo correo) {
-    this.correo = correo;
-  }
-
-  public Direccion getDireccion() throws ConnectionExcep {
-    Direccion temp = direccionService.getByIdPrincipal(usuario.getId());
-    return temp;
-  }
-
-  public void setDireccion(Direccion direccion) {
-    this.direccion = direccion;
-  }
-
-  public Telefono getTelefono() throws ConnectionExcep {
-    return telefonoService.getByIdPrincipal(usuario.getId());
-  }
-
-  public void setTelefono(Telefono telefono) {
-    this.telefono = telefono;
-  }
-
-  public Integer getIdTipoDoc() {
-    return idTipoDoc;
-  }
-
-  public void setIdTipoDoc(Integer idTipoDoc) {
-    this.idTipoDoc = idTipoDoc;
-  }
-
   public List<Usuario> getUsuarios() throws ConnectionExcep {
     try {
       if (Objects.isNull(usuarios)) {
@@ -120,71 +58,7 @@ public class UsuarioController extends CustomController implements Serializable 
     return usuarios;
   }
 
-  public List<TipoDoc> getTipoDocs() {
-    try {
-      if (Objects.isNull(tipoDocs)) {
-        tipoDocs = tipoDocService.getAll();
-      }
-    } catch (ConnectionExcep ex) {
-      System.out.println("Error al consultar los usuarios.....");
-      ex.printStackTrace();
-    }
-    return tipoDocs;
-  }
-
-  public String getOldPassword() {
-    return oldPassword;
-  }
-
-  public void setOldPassword(String oldPassword) {
-    this.oldPassword = oldPassword;
-  }
-
-  public String getNewPassword() {
-    return newPassword;
-  }
-
-  public void setNewPassword(String newPassword) {
-    this.newPassword = newPassword;
-  }
-
-  public String getConfirmPassword() {
-    return confirmPassword;
-  }
-
-  public void setConfirmPassword(String confirmPassword) {
-    this.confirmPassword = confirmPassword;
-  }
   //</editor-fold>
-
-  public void updatePassword() throws ConnectionExcep {
-    if (usuario.verificarContrasena(oldPassword)) {
-      if (newPassword.equals(confirmPassword) && !oldPassword.equals(newPassword)) {
-        usuario.setContrasena(newPassword);
-        usuarioService.update(usuario);
-      }
-    }
-  }
-
-  public void update() throws ConnectionExcep {
-    try {
-      usuarioService.update(usuario);
-    } catch (ConnectionExcep ex) {
-      System.out.println("Error al registrar usuario");
-      ex.printStackTrace();
-    }
-  }
-
-  public void delete() throws ConnectionExcep, IOException {
-    try {
-      usuarioService.delete(loginController.getUsuario());
-      loginController.logout();
-    } catch (ConnectionExcep ex) {
-      System.out.println("Error al registrar usuario");
-      ex.printStackTrace();
-    }
-  }
-
   public void deleteu(Integer idusu) throws ConnectionExcep, IOException {
     try {
       usuarioService.delete(usuarioService.getOne(idusu));
