b91747ba853ec0df589ccc0b30930ce0aa75ae49
danniela <danny94rh@gmail.com>
2021-05-03 19:17:41 -0500

    vistas no usadas

--- src/main/java/tech/seekback/controllers/RegnrefController.java
+++ src/main/java/tech/seekback/controllers/RegnrefController.java
@@ -1,5 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package tech.seekback.controllers;
 
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import tech.seekback.services.ProductoService;
+import javax.ejb.EJB;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import tech.seekback.exceptions.ConnectionExcep;
 import tech.seekback.models.Bodega;
 import tech.seekback.models.BodegaProducto;
@@ -9,21 +25,15 @@ import tech.seekback.models.templates.Timestamps;
 import tech.seekback.services.BodegaProductoService;
 import tech.seekback.services.BodegaService;
 import tech.seekback.services.CategoriasProductoService;
-import tech.seekback.services.ProductoService;
 
-import javax.ejb.EJB;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
-import javax.faces.view.ViewScoped;
-import javax.inject.Named;
-import java.io.IOException;
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
-
+/**
+ *
+ * @author danny
+ */
 @Named
 @ViewScoped
-public class RegnRefController extends CustomController {
+public class RegnrefController implements Serializable {
+
   @EJB
   private ProductoService productoService;
 
@@ -38,29 +48,35 @@ public class RegnRefController extends CustomController {
 
   private Producto producto;
   private BodegaProducto bodegaProducto;
-  private String modelProduct;
-  private String descripcion;
+  private String ModelProduct;
+  private String Descripcion;
   private Double valor;
-  private Integer cant;
-  private List<CategoriasProducto> categoriasProductos;
+  private Integer Cant;
   private Integer idCat;
   private Integer idBod;
+  private List<CategoriasProducto> categoriasProductos;
   private List<Bodega> bodegas;
 
+  public RegnrefController() throws ConnectionExcep {
+    producto = new Producto();
+    bodegaProducto = new BodegaProducto();
+  }
+
+  //<editor-fold defaultstate="collapsed" desc="Getters && Setters">
   public String getModelProduct() {
-    return modelProduct;
+    return ModelProduct;
   }
 
   public void setModelProduct(String ModelProduct) {
-    this.modelProduct = ModelProduct;
+    this.ModelProduct = ModelProduct;
   }
 
   public String getDescripcion() {
-    return descripcion;
+    return Descripcion;
   }
 
   public void setDescripcion(String Descripcion) {
-    this.descripcion = Descripcion;
+    this.Descripcion = Descripcion;
   }
 
   public Double getValor() {
@@ -72,11 +88,11 @@ public class RegnRefController extends CustomController {
   }
 
   public Integer getCant() {
-    return cant;
+    return Cant;
   }
 
   public void setCant(Integer Cant) {
-    this.cant = Cant;
+    this.Cant = Cant;
   }
 
   public Integer getIdCat() {
@@ -87,24 +103,24 @@ public class RegnRefController extends CustomController {
     this.idCat = idCat;
   }
 
-  public Integer getIdBod() {
-    return idBod;
-  }
-
-  public void setIdBod(Integer idBod) {
-    this.idBod = idBod;
-  }
-
   public List<CategoriasProducto> getCategoriasProductos() {
     try {
       if (Objects.isNull(categoriasProductos)) {
         categoriasProductos = categoriasProductoService.getAll();
       }
     } catch (Exception ex) {
-      System.out.println("Error al consultar las categorías de productos...");
+      System.out.println("Error al consultar los Agenda.....");
       ex.printStackTrace();
     }
     return categoriasProductos;
+  }
+
+  public Integer getIdBod() {
+    return idBod;
+  }
+
+  public void setIdBod(Integer idBod) {
+    this.idBod = idBod;
   }
 
   public List<Bodega> getBodegas() {
@@ -115,6 +131,7 @@ public class RegnRefController extends CustomController {
     }
     return bodegas;
   }
+  //</editor-fold>
 
   public void createp() throws ConnectionExcep, IOException {
     // Creación de Timestamp para todos los procesos
@@ -123,8 +140,8 @@ public class RegnRefController extends CustomController {
     timestamps.setCreated_at(momentum);
     timestamps.setUpdated_at(momentum);
 
-    this.producto.setModeloProducto(this.modelProduct);
-    this.producto.setDescripcion(this.descripcion);
+    this.producto.setModeloProducto(this.ModelProduct);
+    this.producto.setDescripcion(this.Descripcion);
     this.producto.setPrecioVenta(this.valor);
     this.producto.setCategoria(categoriasProductoService.getOne(this.idCat));
     this.producto.setEstado(true);
@@ -133,12 +150,12 @@ public class RegnRefController extends CustomController {
 
     this.bodegaProducto.setProducto(producto);
     this.bodegaProducto.setBodega(bodegaService.getOne(this.idBod));
-    this.bodegaProducto.setCantidad(this.cant);
+    this.bodegaProducto.setCantidad(this.Cant);
     this.bodegaProducto.setTimestamps(timestamps);
     this.bodegaProducto = bodegaProductoService.create(bodegaProducto);
 
     System.out.println(
-        "\n\n\n\n\n######################################################################"
+            "\n\n\n\n\n######################################################################"
             + "\n#\t  Registro creado "
             + "\n######################################################################\n");
 
