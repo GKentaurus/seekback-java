5b040cf57752b1e5444f5b34c76858b2e28235a0
Carlos Moreno <gkent@outlook.com>
2021-05-22 17:32:20 -0500

    Puerto SMTP cambiado para GCP

--- src/main/java/tech/seekback/services/tools/MailService.java
+++ src/main/java/tech/seekback/services/tools/MailService.java
@@ -27,7 +27,7 @@ public class MailService {
 
     properties.put("mail.smtp.auth", "true");
     properties.put("mail.smtp.host", "smtp.gmail.com");
-    properties.put("mail.smtp.port", 25);
+    properties.put("mail.smtp.port", 587);
     properties.put("mail.smtp.starttls.enable", "true");
     properties.put("mail.smtp.starttls.required", "false");
     properties.put("mail.smtp.mail.sender", user);
